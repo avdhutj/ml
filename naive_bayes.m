f5a42a5b5b899ca6277ab23898244334d00f6c66
avdhutj <avdhutj@gmail.com>
2013-11-20 18:12:48 -0500

    Save

--- naive_bayes.m
+++ naive_bayes.m
@@ -5,13 +5,20 @@ data = load ("nursery_data.txt");
 num_feat = 8
 num_classes = [3 5 4 4 3 2 3 3 5]
 
+#reset(RandStream.getDefaultStream);
+data = data(randperm(size(data,1)), :);
+train = data(1:size(data, 1)/2, :);
+test = data(size(data,1)/2+1:end,:);
+
+# Training
 prior_class = zeros(num_classes(num_feat+1), 1);
 
-for i = 1:size(data, 1) 
-	prior_class(data(i, num_feat+1)) = prior_class(data(i, num_feat+1)) + 1;
+for i = 1:size(train, 1) 
+	prior_class(train(i, num_feat+1)) = prior_class(train(i, num_feat+1)) + 1;
 end
 
-prior_class = prior_class / size(data, 1);
+prior_class = prior_class / size(train, 1);
 
 prior_class
 
+# Done Training
