3a7ee9b99d665245bc0ef1ae4f96b8975f9be583
avdhutj <avdhutj@gmail.com>
2013-11-20 18:04:18 -0500

    Commiting changes

--- naive_bayes.m
+++ naive_bayes.m
@@ -1,1 +1,17 @@
 # Naive Bayes classifier assignment
+
+data = load ("nursery_data.txt");
+
+num_feat = 8
+num_classes = [3 5 4 4 3 2 3 3 5]
+
+prior_class = zeros(num_classes(num_feat+1), 1);
+
+for i = 1:size(data, 1) 
+	prior_class(data(i, num_feat+1)) = prior_class(data(i, num_feat+1)) + 1;
+end
+
+prior_class = prior_class / size(data, 1);
+
+prior_class
+
